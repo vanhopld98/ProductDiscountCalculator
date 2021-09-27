108d20838afdb65606805fb74244688dc4f4f599
vanhopld98 <vanhopld98@gmail.com>
2021-09-27 14:41:36 +0700

    done

---
+++ src/main/java/DiscountServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "DiscounntServlet", value = "/convert")
+public class DiscountServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int listPrice = Integer.parseInt(request.getParameter("listPrice"));
+        int discountPercent = Integer.parseInt(request.getParameter("discountPercent"));
+
+        double DiscountAmount = listPrice * discountPercent * 0.01;
+        double DiscountPrice = listPrice - DiscountAmount;
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Discount Amount:" + DiscountAmount + "</h1>");
+        writer.println("<h1>Discount Price:" + DiscountPrice + "</h1>");
+        writer.println("</html>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,36 @@
+  <%--
+  Created by IntelliJ IDEA.
+  User: Van Hop
+  Date: 9/27/2021
+  Time: 2:17 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Product Discount Calculator</title>
+    <style type="text/css">
+      .login {
+        height:180px; width:230px;
+        margin:0;
+        padding:10px;
+        border:1px #CCC solid;
+      }
+      .login input {
+        padding:5px; margin:5px
+      }
+    </style>
+  </head>
+  <body>
+  <h2>Product Discount Calculator</h2>
+  <form action="/convert"  method="post" class="login">
+    <label>Product Description: </label>
+    <input type="text" name="productDescription" placeholder="Aa"><br>
+    <label>List Price: </label>
+    <input type="text" name="listPrice" placeholder="Aa"><br>
+    <label>Discount Percent: </label>
+    <input type="text" name="discountPercent" placeholder="Aa"><br>
+    <input type="submit" value="Calculate Discount">
+  </form>
+  </body>
+</html>
